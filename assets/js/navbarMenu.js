0115124b1a75505c94da3b7ce8e03c4465063e29
lakshaysachdeva01 <sachdevalaksh001@gmail.com>
2024-09-10 19:09:56 +0530

    Add files via upload

---
+++ assets/js/navbarMenu.js
@@ -0,0 +1,9 @@
+const navbarOverlay = document.getElementById("navbar__offcanvas");
+
+function openNavbarMenu() {
+  navbarOverlay.style.display = "block";
+}
+function closeNavbarMenu() {
+  navbarOverlay.style.display = "none";
+}
+
---
+++ menu.js
@@ -0,0 +1,26 @@
+document.getElementById("open").addEventListener("click" , openmenu());
+
+function openmenu(){
+    showmenu.style.display= "block";
+   
+  //  logo.style.display="none";
+   
+    
+}
+
+
+
+document.getElementById("close").addEventListener("click",closemenu());
+
+function closemenu() {
+
+    showmenu.style.display="none";
+   // openn.style.display="none";
+   // logo.style.display="block";
+    
+   
+}
+
+
+
+
